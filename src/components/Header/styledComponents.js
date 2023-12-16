2fdfd761463852f803bacefea49291b34bb1d726
shanmukheswarikondeti <eswarikondeti@gmail.com>
2023-12-16 15:18:43 +0000

    projects showcase

---
+++ src/components/Header/styledComponents.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  background-color: HeaderContainer;
+`
+export const Image = styled.img`
+  margin-top: 15px;
+  margin-bottom: 15px;
+  width: 200px;
+  margin-left: 50px;
+`
---
+++ src/components/ProjectCard/styledComponents.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const ProjectCardListItem = styled.li`
+  list-style-type: none;
+  height: 260px;
+  width: 260px;
+  background-color: #ffffff;
+  border-radius: 15px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  border: 1px solid #cbd5e1;
+`
+export const ProjectCardImage = styled.img`
+  width: 100%;
+  height: 200px;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+`
+export const ProjectCardName = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  font-family: 'Roboto';
+  padding-left: 20px;
+`
